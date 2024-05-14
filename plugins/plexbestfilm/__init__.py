e03fcafae28eddcd1f6e4f73fe3fc643462b4b1a
trott90 <trott90@gmail.com>
2024-05-14 20:08:31 +0800

    Update __init__.py

--- plugins/plexbestfilm/__init__.py
+++ plugins/plexbestfilm/__init__.py
@@ -40,7 +40,7 @@ class PlexBestFilm(_PluginBase):
     # 作者主页
     author_url = "https://github.com/developer-wlj"
     # 插件配置项ID前缀
-    plugin_config_prefix = "plexbestfil_"
+    plugin_config_prefix = "plexbestfilm_"
     # 加载顺序
     plugin_order = 13
     # 可使用的用户级别
